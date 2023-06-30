0cfda9d5b99daf0ea644754ac1d558ad41013a6b
Imoh Precious <jaypee88830@gmail.com>
2023-06-30 23:24:15 +0100

    styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
-// import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
 import Rating from '@material-ui/lab'
 import useStyles from './styles'
 
 const Map = () => {
+	// GOOGLE MAPS API KEY = AIzaSyDhKS2xj_QcvtA1o2l3d-kD_PDwNT4s1KI
+	// GOOGLE MAP API = AIzaSyA0oYubMrdb6DLd9iXF1Kr__IbIVoAU7MY
+
 	const classes = useStyles()
 	const isMobile = useMediaQuery('(min-width:600px)')
 	const coordinates = { lat: 0, lng: 0 }
@@ -13,7 +16,7 @@ const Map = () => {
 	return (
 		<div className={classes.mapContainer}>
 			<GoogleMapReact
-				bootstrapURLKeys={{ key: '' }} 
+				bootstrapURLKeys={{ key: 'AIzaSyA0oYubMrdb6DLd9iXF1Kr__IbIVoAU7MY' }} 
 				defaultCenter={coordinates}
 				center={coordinates}
 				defaultZoom={14}
